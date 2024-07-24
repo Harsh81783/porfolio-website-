2783acc13df254c0d928c8b3d23061c135403ff2
Harsh <131382585+Harsh81783@users.noreply.github.com>
2024-07-24 07:53:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add event listeners to navigation links
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView
